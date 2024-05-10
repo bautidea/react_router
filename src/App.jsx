fcb637b6966c166bdb08c4800814e9b6fa1b55d5
bautidea <juanbautistadeangelis@gmail.com>
2024-05-10 15:14:09 -0300

    Starting with an easy routing.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,35 @@
-function App() {
+import { useState } from 'react';
+
+function HomePage() {
   return (
     <>
-      <h1>HI</h1>
+      <h1>Home</h1>
+      <p>
+        This is a example page to be used for creating React Router from scratch
+      </p>
+      <a href="/about">About Me</a>
     </>
+  );
+}
+
+function AboutPage() {
+  return (
+    <>
+      <h1>About</h1>
+      <p>Creating a clone of React Router</p>
+      <a href="/">Home</a>
+    </>
+  );
+}
+
+function App() {
+  const [currentPath, setCurrentPath] = useState(window.location.pathname);
+
+  return (
+    <main>
+      {currentPath === '/' && <HomePage />}
+      {currentPath === '/about' && <AboutPage />}
+    </main>
   );
 }
 
