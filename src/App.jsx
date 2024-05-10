a850e7075c401761dbd4fcd4623539851f80797e
bautidea <juanbautistadeangelis@gmail.com>
2024-05-10 15:56:56 -0300

    Starting with SPA routing.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
 import { useState } from 'react';
 
+// The Best from to perform a SPA, is by creating a function called 'navigate' that takes 'href' as a
+// parameter.
+function navigate(href) {
+  // Here we use the 'history' property, provides a form of manipulating browser history (pages visited).
+  // Then  we use the 'pushState()' method of 'history' to add an entry to the browser session.
+  // We perform this to change the URL, but we dont refresh the page, we just update the URL.
+  window.history.pushState({}, '', href);
+
+  // Creating a custom Event to warn, that i've changed the URL, because there isn't a native way for us
+  // to hear the '.pushState()' event (we can't hear a navigation when going forward)
+  // In JS we can create custom events like this to hear when a navigation goes backward.
+  const navigationEvent = new Event('pushState');
+
+  // Now i have to sent the event.
+  window.dispatchEvent(navigationEvent);
+}
+
 function HomePage() {
   return (
     <>
@@ -24,7 +41,7 @@ function AboutPage() {
 
 function App() {
   const [currentPath, setCurrentPath] = useState(window.location.pathname);
-
+  console.log(currentPath);
   return (
     <main>
       {currentPath === '/' && <HomePage />}
